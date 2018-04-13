81793f6210e6c91f6fec84145c3b2cd2bb222ad7
Hasmik <hasmik2011@gmail.com>
2018-04-13 15:01:52 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+	setup();
+
+
+
+function setup(){
+	
+	var $passage = $('#weirdtext');
+	
+	//get the inner HTML of the #weirdtext paragraph
+	var rawtxt = $passage.html();
+	
+	//Get the length of the string for use in loop
+	var len = rawtxt.length;
+	
+	//empty string used to store final text that includes spans
+	var newtext = '';
+
+	
+	//For each character inside #weirdtext string (this is why we got length)
+	for(var i = 0; i < len; i ++){
+		
+		//get a random num between 1 and 5
+		var rng = Math.floor(Math.random() * 5) + 1;
+		
+		//get the i-th character from the string
+		var currentchar = rawtxt.charAt(i);
+		if(currentchar == ' '){
+			//if it's a space, add an empty .space span
+			var newchar = '<span class="space"></span>';
+		}
+		else{
+			//otherwise, wrap it with a span, and give it class effectN, where N is a random int as before
+			var newchar = '<span class="effect' + rng + '">' + currentchar + '</span>';
+		}
+		//add this new "char" (actually it's a char with spans wrapping it) to the empty string
+		newtext = newtext + newchar;
+	}
+	
+	//replace #weirdtext paragraphs inner HTML with the newly created string
+	$passage.html(newtext);
+	
+}
+
+let spans = document.querySelectorAll('.word span');
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
+
+ // Configure/customize these variables.
+
+
+
+
+
+    
+
+});
+
+
+
